d1d4aaa9463bb8f580849a7da1e5f32f6fd52ffd
aRinak <grisina047@gmail.com>
2022-06-08 23:16:44 +0300

    Проба плагина spotbugs

--- src/test/java/ru/netology/statistic/StatisticServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticServiceTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class statisticservicetest {
+class StatisticServiceTest {
     @Test
     void findMax() {
         StatisticService service = new StatisticService();
